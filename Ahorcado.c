e37e29730f0cd8553e242c7a2fabcb7be1686493
xXRubio10Xx <rubiosalvadormarti@gmail.com>
2024-11-03 23:55:31 +0100

    Final

--- Ahorcado.c
+++ Ahorcado.c
@@ -1,183 +1,170 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
-const char player1[8] = "\033[92m"; //Verde
-const char player2[8] = "\033[94m"; //Cyan
-const char reset[8] = "\033[37m";   //Blanco
+void map_print();
+void map_update();
+void hang_print();
+void hang_update();
 
-void print_scr();
-void update_scr();
-void scan_scr();
-void bot_start();
-void bot_logic();
+int lives = 6;
+char list [15][20] = {"billar", "pizarra", "fuego", "anaconda", "sardina", "puerta", "gafas", "enchufe", "sillon", "patinete", "camiseta", "balcon", "telefono", "perro", "sudadera"};
+char *word;
+int guess [20];
+char letters [27];
+int win = 0;
+char hangman [7][9] =   {' ',' ',' ','+','-','-','-','+',' ',
+                         ' ',' ',' ','|',' ',' ',' ','|',' ',
+                         ' ',' ',' ','O',' ',' ',' ','|',' ',
+                         ' ',' ','/','|','\\',' ',' ','|',' ',
+                         ' ',' ','/',' ','\\',' ',' ','|',' ',
+                         ' ',' ',' ',' ',' ',' ',' ','|',' ',
+                         '=','=','=','=','=','=','=','=','='};
 
-int board [3][3] = {{0,0,0},
-                    {0,0,0},
-                    {0,0,0}};
-int pos_temp[9][2];
-int tempx;
-int tempy;
-int result = 0;
-int player = 1;
-
+int board [10][10];
+int posx;
+int posy;
 int main()
 {
     srand(time(NULL));
-    bot_start();
+    do {
+    posx = rand() % 10;
+    posy = rand() % 10;
+    }while (posx == 4 && posy == 4);
 
-    while (result == 0)
+    //MAP
+    while (posx != 4 || posy != 4)
     {
-        print_scr();
-        //update_scr();
-        bot_logic();
-        scan_scr();
-        system("pause");
+        map_print();
+        map_update();
+    }
+
+    //HANGMAN
+    system("cls");
+    memset(letters, ' ', sizeof(letters));
+    word = list[rand() % 15]; //Puntero
+    hang_print();
+    while (lives > 0 && win < strlen(word)){
+        hang_update();
+        hang_print();
     }
     system("cls");
-    if (result == 1)
-    {
-        printf (player1);
-        printf ("Ha ganado el jugador 1!\n\n");
-        printf (reset);
-    }else if (result == 2)
-    {
-        printf (player2);
-        printf ("Ha ganado el jugador 2!\n\n");
-        printf (reset);
+    if (win == strlen(word)) {
+        printf("Has ganado\n\nla palabra era: %s\n", word);
+    } else {
+        printf("Has perdido\n\nla palabra era: %s\n", word);
     }
     system("pause");
     return 0;
 }
 
-void print_scr(){
+void map_print(){
     system("cls");
-    if (player == 1)
-    {
-        printf (player1);
-        printf ("Jugador 1\n");
-        printf (reset);
-    }else if (player == 2)
-    {
-        printf (player2);
-        printf ("Jugador 2\n");
-        printf (reset);
-    }
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < 10; i++)
     {
         printf ("\n");
-        if (i != 0)
-            printf ("%d " , i);
-        for (int l = 0; l < 4; l++)
+        for (int l = 0; l < 10; l++)
         {
-            if (i == 0 && l != 0)
-                printf (" %d" , l);
-            else if (i == 0 && l == 0)
-                printf ("  ");
-            else if (l != 0)
-                printf ("|");
-            if (board[i-1][l-1] == 1 && i != 0 && l != 0)
-            {
-                printf (player1);
-                printf ("X");
-                printf (reset);
-                }
-            else if (board[i-1][l-1] == 2 && i != 0 && l != 0)
-            {
-                printf (player2);
-                printf ("O");
-                printf (reset);
+            if (i == 4 && l == 4){
+                board[i][l] = 2;
+                printf ("R");
             }
-            else if (board[i-1][l-1] == 0 && i != 0 && l != 0)
-                printf ("_");
-        }
-        if (i != 0)
+            else if (i == posy && l == posx){
+                board[i][l] = 1;
+                printf ("X");
+            }
+            else{
+                board[i][l] = 0;
+                printf ("-");
+            }
+        }     
+    }
+    printf ("\n");
+}
+
+void map_update(){
+    char select;
+    printf ("\n");
+    printf ("W = Arriba\n");
+    printf ("A = Izquierda\n");
+    printf ("S = Abajo\n");
+    printf ("D = Derecha\n");
+    scanf_s (" %c", &select);
+    int newPosX = posx, newPosY = posy;
+    switch (select) {
+        case 'w': case 'W': newPosY -= 1; break;
+        case 'a': case 'A': newPosX -= 1; break;
+        case 's': case 'S': newPosY += 1; break;
+        case 'd': case 'D': newPosX += 1; break;
+        default: break;
+    }
+
+    if (newPosX >= 0 && newPosX < 10 && newPosY >= 0 && newPosY < 10) {
+        posx = newPosX;
+        posy = newPosY;
+    }
+}
+
+void hang_print(){
+    system("cls");
+    for (int i = 0; i < 7; i++)
+    {
+        printf ("\n");
+        for (int l = 0; l < 9; l++)
         {
-            printf ("|");
+            if ((i == 2 && l == 3 && lives >= 6) ||   // Cabeza
+                (i == 3 && l == 3 && lives >= 5) ||   // Cuerpo
+                (i == 3 && l == 2 && lives >= 4) ||   // Brazo iz
+                (i == 3 && l == 4 && lives >= 3) ||   // Brazo de
+                (i == 4 && l == 2 && lives >= 2) ||   // Pierna iz
+                (i == 4 && l == 4 && lives >= 1)) {   // Pierna de
+                printf(" ");
+            } else {
+                printf("%c", hangman[i][l]);
+            }
         }
     }
     printf ("\n");
 }
 
-void update_scr(){
-    int err = 0;
-    do{    
-        int x,y;
-        printf ("\n");
-        if (err == 1)
-        {
-            printf ("coordenada incorrecta\n");
-        }
-        printf ("Introduce la coordenada (X): ");
-        scanf_s ("%d", &x);
-        printf ("Introduce la coordenada (Y): ");
-        scanf_s ("%d", &y);
-        if (board[x-1][y-1] != 0)
-            err = 1;
-        else {
-            board[x-1][y-1] = player;
-            err = 0;
-        }
-        if (player == 1 && err == 0)
-            player = 2;
-        else if (player == 2 && err == 0)
-            player = 1;
-    }while (err != 0);
-}
-
-void scan_scr(){
-    for (int i = 0; i < 3; i++)
+void hang_update(){
+    char try;
+    int correct = 0;
+    
+    for (int i = 0; i < strlen(word); i++)
     {
-        if (board[i][0] == 1 && board[i][1] == 1 && board[i][2] == 1 || board[0][i] == 1 && board[1][i] == 1 && board[2][i] == 1)
-            result = 1;
-        else if (board[i][0] == 2 && board[i][1] == 2 && board[i][2] == 2 || board[0][i] == 2 && board[1][i] == 2 && board[2][i] == 2)
-            result = 2;
+        if (guess[i] == 1)
+            printf ("%c", word[i]);
+        else
+            printf ("_");
     }
-    if (board[0][0] == 1 && board[1][1] == 1 && board[2][2] == 1 || board[0][2] == 1 && board[1][1] == 1 && board[2][0] == 1)
-        result = 1;
-    else if (board[0][0] == 2 && board[1][1] == 2 && board[2][2] == 2 || board[0][2] == 2 && board[1][1] == 2 && board[2][0] == 2)
-        result = 2;
-}
-
-void bot_start(){
-    int pos = 0;
-    for (int i = 0; i < 3; i++) {
-        for (int l = 0; l < 3; l++) {
-            if (board[i][l] == 0)
-                pos_temp[pos][0] = i;
-                pos_temp[pos][1] = l;
-                pos++;
+    printf("\n\nLetras usadas: ");
+    for (int i = 0; i < 27; i++) {
+        if (letters[i] != ' ') {
+            printf("%c ", letters[i]);
         }
     }
-    if (pos > 0) {
-        int rand_n = rand() % pos;
-        tempx = pos_temp[rand_n][0];
-        tempy = pos_temp[rand_n][1];
-        board[tempx][tempy] = 2;
-    }
-}
-
-void bot_logic(){
-    int pos = 0;
-    for (int i = -1; i <= 1; i++) {
-        for (int l = -1; l <= 1; l++) {
-            if (board[tempx + i][tempy + l] == 0 && tempx + i >= 0 && tempy + l >= 0 && tempx + i <= 2 && tempy + l <= 2){
-                pos++;
-            }
+    printf("\n\nIntroduce una letra: ");
+    scanf(" %c", &try);
+    for (int i = 0; i < strlen(word); i++) {
+        if (strncmp(&try, &word[i], 1) == 0 && guess[i] == 0) {
+            guess[i] = 1;
+            correct = 1;
+            win += 1;
         }
     }
-    for (int i = 0; i < 3; i++) {
-        for (int l = 0; l < 3; l++) {
-            if (board[i][l] == 0)
-                pos_temp[pos][0] = i;
-                pos_temp[pos][1] = l;
-                pos++;
+    for (int i = 0; i < 27; i++) {
+        if (letters[i] == try) {
+            return;
         }
     }
-    if (pos > 0) {
-        int rand_n = rand() % pos;
-        tempx = pos_temp[rand_n][0];
-        tempy = pos_temp[rand_n][1];
-        board[tempx][tempy] = 2;
+    for (int i = 0; i < 27; i++) {
+        if (letters[i] == ' ') {
+            letters[i] = try;
+            break;
+        }
     }
+    if (!correct)
+        lives -= 1;
 }
